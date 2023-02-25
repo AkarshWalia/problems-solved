74b88f77ef183ecd1314bd91de05e67424eab5a5
AkarshWalia <118650755+AkarshWalia@users.noreply.github.com>
2023-02-25 18:40:52 +0530

    Time: 11 ms (48.20%), Space: 13.8 MB (24.47%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        
-   vector<int> v;
-
-    int f=-1,l=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                if(f==-1){
-                    f=i;
-                    l=i;
-                }
-                else{
-                    l=i;
-                }
+    int binaryfirst(vector<int>& nums, int target, int n) {
+        int s = 0, e = n - 1, a = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                a = mid;
+                e = mid - 1;
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
-       v.push_back(f);
-        v.push_back(l);
-        return v;
+        return a;
+    }
+
+    int binarylast(vector<int>& nums, int target, int n) {
+        int s = 0, e = n - 1, a = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                a = mid;
+                s = mid + 1;
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return a;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        int a = binaryfirst(nums, target, n);
+        int b = binarylast(nums, target, n);
+        ans.push_back(a);
+        ans.push_back(b);
+        return ans;
     }
 };
+
 

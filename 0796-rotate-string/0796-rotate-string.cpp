584a0b4de069dcf006c61405799eac4c44bd35d4
AkarshWalia <118650755+AkarshWalia@users.noreply.github.com>
2023-02-27 18:38:45 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (22.34%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -8,14 +8,14 @@ public:
         string ans = s + s;
         int n = ans.length();
         int m = goal.length();
-        bool found = false;
+        
         for (int i = 0; i < n - m + 1; i++) {
             if (ans.substr(i, m) == goal) {
-                found = true;
-                break;
+                return true;
+                
             }
         }
-        return found;
+        return false;
 
     }
 };
